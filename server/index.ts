7d145ce04d2da1223400e4d7cc29d48f78fc3b4a
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-09-01 12:27:22 +0100

    add cors

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,34 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import dotenv from 'dotenv';
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+
 
 dotenv.config();
 
 const app = express();
+
+
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: { origin: "*", credentials: true },
+});
 
 // Serve static files (if needed)
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Define a route
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Socket.io server!" });
 });
 
 // Socket.io event handling
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 
   // Add your Socket.io event handlers here
